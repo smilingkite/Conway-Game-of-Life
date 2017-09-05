4e6c76786edbb5991f0c35e2d4bc5a7ba220b247
smilingkite <mail@katinkahesselink.net>
2017-09-05 11:08:09 +0200

    collecting the rules, trying things out

--- game.js
+++ game.js
@@ -2,3 +2,19 @@ $('td').on('click', function(e) {
 	$(this).toggleClass('selected'); //you can list several class names
 	e.preventDefault();
 });
+
+function getTableData(table) {
+	var data = [];
+	table.find('tr').each(function(rowIndex, r) {
+		var cols = [];
+		$(this)
+			.find('td')
+			.each(function(colIndex, c) {
+				cols.push(c.hasClass('selected'));
+			});
+		data.push(cols);
+	});
+	return data;
+}
+
+console.log(getTableData);
